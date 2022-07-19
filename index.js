c6d2cb8f1bc23cf7030c04909e29ecd953c51fd6
PythonGenzo <sanprasanth94@gmail.com>
2022-07-19 08:32:42 +0530

    all done

--- index.js
+++ index.js
@@ -16,27 +16,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 
-const classData =[
-    {
-      "id":"1",
-      "class":"1",
-      "class-title":"Javascript-Day-1:Introduction to Browser and web",
-      "date&time":"15/02/2022 - Tuesday-10.30AM:1.00PM",
-      "contents":"intro to web , browser wars, v8 engine",
-      "pre-read":"np pre-read available",
-      "activities":"www.google.com"
-    },
-    {
-      "id":"2",
-      "class":"2",
-      "class-title":"Javascript-Day-1:Introduction to Browser and web",
-      "date&time":"16/02/2022 - wednesday-10.30AM:1.00PM",
-      "contents":"intro to web , browser wars, v8 engine",
-      "pre-read":"np pre-read available",
-      "activities":"www.google.com"
-    }
-
-  ];
 
 const MONGO_URL =  process.env.MONGO_URL;
 
@@ -74,16 +53,13 @@ app.get("/classData/:id",  async function(req, res) {
         : res.status(404).send({ msg: "zNo such class data found"});
 });
 
-app.delete("/classData/:id",  async function(req, res) {
-    const { id } = req.params;
-    const sessionData = await client
+app.post("/classData", async function (req, res) {
+    const data = req.body;
+    const result = await client
         .db("B33WD")
         .collection("classData")
-        .deleteOne({id: id})
-
-    sessionData.deletedCount > 0
-        ? res.send(sessionData)
-        : res.status(404).send({ msg: "No such class data found"});
+        .insertMany(data);
+    res.send(result);
 });
 
 app.put("/classData/:id",  async function(req, res) {
@@ -97,14 +73,21 @@ app.put("/classData/:id",  async function(req, res) {
          res.send(result);
 });
 
-app.post("/classData", async function (req, res) {
-    const data = req.body;
-    const result = await client
+app.delete("/classData/:id",  async function(req, res) {
+    const { id } = req.params;
+    const sessionData = await client
         .db("B33WD")
         .collection("classData")
-        .insertMany(data);
-    res.send(result);
+        .deleteOne({id: id})
+
+    sessionData.deletedCount > 0
+        ? res.send(sessionData)
+        : res.status(404).send({ msg: "No such class data found"});
 });
+
+
+
+
 
 
 app.listen(PORT, () => console.log(`App is started in ${PORT}`));;
